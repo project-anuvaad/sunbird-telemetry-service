e3c5f9fa60a6be3b3e4bcdd82693aea20a88cb65
Sivaprakash <sivaprakash.ramasamy@tarento.com>
2021-02-19 13:16:06 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const createAppServer = () => {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,' + 'cid, user-id, x-auth, Cache-Control, X-Requested-With, datatype, *')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,' + 'cid, user-id, x-auth, x-app-id, Cache-Control, X-Requested-With, datatype, *')
     if (req.method === 'OPTIONS') res.sendStatus(200)
     else next()
   })
